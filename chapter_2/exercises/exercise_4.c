4ca99e92dcb10eba038910092adfe76bbefbb2ac
chinhungtseng <bluesshown@gmail.com>
2019-08-19 21:38:10 +0800

    finish chapter 3 exercises

---
+++ chapter_2/exercises/exercise_4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int value1, value2, sum;
+
+    value1 = 15;
+    value2 = 87;
+    sum = value1 + value2;
+    printf("The sum of %i and %i is %i\n", value1, value2, sum);
+
+    return(0);
+}
